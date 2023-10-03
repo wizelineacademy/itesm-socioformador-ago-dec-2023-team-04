51cb225983181dcd7dcb449d5ba965a2a022eae6
Raúl <a01197655@tec.mx>
2023-10-03 09:05:51 -0600

    StudentDataView
    basic StudentDataView component
    duplicated file erased

--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import StudentsTable from '@/components/students-table.tsx';
+import StudentDataView from '@/components/student-data-view.tsx';
 import {getAllStudents} from '@/lib/student.ts';
-import Icon from '@/components/icon.tsx';
 
 export default async function Students() {
 	const students = await getAllStudents();
 	return (
 		<main className='w-full gb-stone-900'>
-			<div className='p-16 text-stone-300 min-h-screen'>
+			<div className='p-16 text-stone-300 min-h-screen flex flex-col'>
 				<h1 className='text-4xl'>Alumnos</h1>
 
-				<div className='pt-8 min-h-full grid gap-x-6 grid-cols-2 grid-cols[80%_20%] grid-rows-1 justify-items-stretch'>
-					<div className='p-6 rounded-lg bg-stone-800'>
+				<div className='pt-8 flex gap-4 grow'>
+					<div className='p-6 w-auto rounded-lg bg-stone-800 flex-1'>
 						<StudentsTable students={students} className='w-full'/>
 					</div>
-					<div className='p-12 rounded-lg bg-stone-800 text-center text-base align-middle'>
-						<Icon name='person' className='m-2'/>
-						<p>Ningún alumno seleccionado.
-							Seleccione a un alumno para mostrar su información
-						</p>
-					</div>
+					<StudentDataView className='p-12 w-96 h rounded-lg bg-stone-800 text-center text-base flex justify-center flex-col items-center'/>
 				</div>
 			</div>
 		</main>
