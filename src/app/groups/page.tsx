8d0f9f2e44a8fa26d66fc4236f8df61f6f531421
Andrea Garza <80288669+andreaagarzaa@users.noreply.github.com>
2023-10-03 09:04:35 -0600

    Update Groups

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -2,17 +2,20 @@ import React from "react";
 
 export default function Home() {
     return (
-        <main className="w-full">
-            <h1 className="text-4xl mb-4 text-slate-100 color: rgb(212 212 216) decoration-2 pt-3 padding-top: px-14rem">
+        <main className="w-full md: w-auto">
+            <div className="p-16 text-stone-300 min-h-screen flex flex-col">
+
+            <h1 className="text-4xl mb-4 text-slate-200 ">
                 Grupos
             </h1>
-            <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-2">
-                <div className="mr-4 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-gray-300">
+                <div className="justify-center">
+            <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-4 flex-1 h-36 w-364 ">
+                <div className="mr-2 block rounded-sm bg-stone-800 shrink-0 h-36  ">
                     <div className="p-6">
-                        <h5 className="mb-2 text-xl font-medium leading-tight text-center">
+                        <h5 className="mb-2 text-xl font-medium leading-tight text-center text-gray-200">
                             Grupo A
                         </h5>
-                        <div className="flex items-center justify-center">
+                        <div className="flex items-center justify-center text-gray-300">
                             <p className="mb-2 text-base mr-28">
                                 6
                             </p>
@@ -20,7 +23,7 @@ export default function Home() {
                                 1
                             </p>
                         </div>
-                        <div className="flex items-center justify-center">
+                        <div className="flex items-center justify-center text-stone-400">
                             <p className="mb-2 text-base mr-12">
                                 Alumnos
                             </p>
@@ -30,23 +33,79 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-gray-300">
+                <div className="mr-2 block rounded-sm bg-stone-800 shrink-0 h-36  ">
                     <div className="p-6">
-                        <h5 className="mb-2 text-xl font-medium leading-tight text-center">
+                        <h5 className="mb-2 text-xl font-medium leading-tight text-center text-gray-200">
                             Grupo B
                         </h5>
-                        <div className="flex items-center justify-center">
+                        <div className="flex items-center justify-center text-gray-300">
+                            <p className="mb-2 text-base mr-28">
+                                6
+                            </p>
                             <p className="mb-2 text-base">
-                                Alumnos
+                                1
                             </p>
                         </div>
-                        <div className="flex items-center justify-center">
+                        <div className="flex items-center justify-center text-stone-400">
+                            <p className="mb-2 text-base mr-12">
+                                Alumnos
+                            </p>
                             <p className="mb-2 text-base">
                                 Profesor(es)
                             </p>
                         </div>
                     </div>
                 </div>
+                <div className="mr-2 block rounded-l-sm bg-stone-800 shrink-0 h-36  ">
+                    <div className="p-6">
+                        <h5 className="mb-2 text-xl font-medium leading-tight text-center text-gray-200">
+                            Grupo C
+                        </h5>
+                        <div className="flex items-center justify-center text-gray-300">
+                            <p className="mb-2 text-base mr-28">
+                                6
+                            </p>
+                            <p className="mb-2 text-base">
+                                1
+                            </p>
+                        </div>
+                        <div className="flex items-center justify-center text-stone-400">
+                            <p className="mb-2 text-base mr-12">
+                                Alumnos
+                            </p>
+                            <p className="mb-2 text-base">
+                                Profesor(es)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="mr-2 block rounded-sm bg-stone-800 shrink-0 h-36  ">
+                    <div className="p-6">
+                        <h5 className="mb-2 text-xl font-medium leading-tight text-center text-gray-200">
+                            Grupo D
+                        </h5>
+                        <div className="flex items-center justify-center text-gray-300">
+                            <p className="mb-2 text-base mr-28">
+                                6
+                            </p>
+                            <p className="mb-2 text-base">
+                                1
+                            </p>
+                        </div>
+                        <div className="flex items-center justify-center text-stone-400">
+                            <p className="mb-2 text-base mr-12">
+                                Alumnos
+                            </p>
+                            <p className="mb-2 text-base">
+                                Profesor(es)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                </div>
+
             </div>
         </main>
     );
