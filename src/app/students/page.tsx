041aea7ffaa26793079f12ccd551dd50a80b440c
Raul Isai Murillo Aleman <105447584+RaulMurilloA@users.noreply.github.com>
2023-10-02 18:35:56 -0600

    Update page.tsx

--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import clsx from 'clsx';
-import Icon from '@/components/icon.tsx';
-import StudentCreationForm from '@/app/students/create/student-creation-form.tsx';
+import StudentsTable from '@/components/StudentsTable';
+import {getAllStudents} from '@/lib/student';
+import Icon from "@/components/Icon";
 
-export default function StudentPage() {
-	return (
-		<div className='flex flex-col justify-center text-center items-center w-full h-full text-stone-400'>
-			<Icon name='person' className='m-2'/>
-			<p>Ningún alumno seleccionado.
-				Seleccione a un alumno para mostrar su información
-			</p>
-		</div>
-	);
+export default async function Students() {
+    const students = await getAllStudents();
+    return <main className="w-full gb-stone-900">
+        <div className={"p-16 text-stone-300 min-h-screen"}>
+            <h1 className="text-4xl">Alumnos</h1>
+
+            <div className={"pt-8 min-h-full grid gap-x-6 grid-cols-2 grid-rows-1 first:col- last:col- justify-items-stretch"}>
+                <div className={"p-6 rounded-lg bg-stone-800"}>
+                    <StudentsTable students={students} className="w-full"/>
+                </div>
+                <div className="p-12 rounded-lg bg-stone-800 text-center text-base align-middle">
+                    <Icon name="person" className="m-2"/>
+                    <p>Ningún alumno seleccionado.
+                        Selecione a un alumno para mostrar su información</p>
+                </div>
+            </div>
+        </div>
+    </main>
 }
-
