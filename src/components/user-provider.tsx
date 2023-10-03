fa78c7aef23e6642d5bb59d7abb80fb9fc209ad4
Hiram Muñoz <hirammunozr@outlook.com>
2023-10-03 08:22:35 -0600

    fixed linting issue

--- src/components/user-provider.tsx
+++ src/components/user-provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import {UserProvider as BaseUserProvider} from '@auth0/nextjs-auth0/client.js';
+import {UserProvider as BaseUserProvider} from '@auth0/nextjs-auth0/client';
 
 export default function UserProvider({children}: {readonly children: React.ReactNode}) {
 	return (
