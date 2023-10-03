d45b9495dcb1d146e55ba1a2855024c96775ba65
Andrea Garza <80288669+andreaagarzaa@users.noreply.github.com>
2023-10-03 09:56:41 -0600

    Update Groups

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 export default function Home() {
     return (
-        <main className="w-full md: w-auto">
+        <main>
             <div className="p-16 text-stone-300 min-h-screen flex flex-col">
 
             <h1 className="text-4xl mb-4 text-slate-200 ">
                 Grupos
             </h1>
                 <div className="justify-center">
+
             <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-4 flex-1 h-36 w-364 ">
                 <div className="mr-2 block rounded-sm bg-stone-800 shrink-0 h-36  ">
                     <div className="p-6">
