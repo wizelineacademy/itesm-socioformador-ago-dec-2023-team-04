7ba6c8dc93c31225bf3eedd3415e9d089336d376
Raúl <a01197655@tec.mx>
2023-10-03 09:05:51 -0600

    StudentDataView
    basic StudentDataView component
    duplicated file erased

---
+++ src/components/student-data-view.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import clsx from 'clsx';
+import Icon from '@/components/icon.tsx';
+
+export default function StudentDataView({className}: {readonly className?: string}) {
+	return (
+		<div className={clsx(className)}>
+			<Icon name='person' className='m-2'/>
+			<p>Ningún alumno seleccionado.
+				Seleccione a un alumno para mostrar su información
+			</p>
+		</div>
+
+	);
+}
