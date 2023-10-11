4ce2e4c48fb2a0f0b562d4167002acbda1196559
Erick Hernández <erickhdz17@gmail.com>
2023-10-11 08:58:11 -0600

    Student creation error fixed

--- src/app/students/create/student-creation-form.tsx
+++ src/app/students/create/student-creation-form.tsx
@@ -1,74 +1,121 @@
 'use client';
 import React, {useState} from 'react';
-import {ZodError} from 'zod';
+import {useRouter} from 'next/navigation';
+import {useFormik} from 'formik';
+import {toFormikValidate} from 'zod-formik-adapter';
 import BiometricDataDialog from './biometric-data-dialog.tsx';
-import {LabeledInput} from '@/components/labeled-input.tsx';
 import {Button} from '@/components/button.tsx';
-import {decodeForm} from '@/lib/schemas/util.ts';
-import {studentRegistrationSchema} from '@/lib/schemas/student.ts';
+import {type StudentRegistration, studentRegistrationSchema} from '@/lib/schemas/student.ts';
 import createStudent from '@/app/students/create/create-student-action.ts';
 import ButtonModalTrigger from '@/components/button-modal-trigger.tsx';
+import TextField from '@/components/text-field.tsx';
+import Icon from '@/components/icon.tsx';
 
 export default function StudentCreationForm({className}: {readonly className?: string}) {
 	const [issues, setIssues] = useState(new Map<string, string>());
 	const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
-	const [biometricData, setBiometricData] = useState<number[]>();
+	const router = useRouter();
 
-	const handleForm = async (formData: FormData) => {
-		try {
-			await decodeForm(formData, studentRegistrationSchema);
-			setIssues(new Map());
-
-			const result = await createStudent(formData);
-
+	const formik = useFormik<StudentRegistration>({
+		initialValues: {
+			registration: '',
+			givenName: '',
+			familyName: '',
+			biometricData: [],
+		},
+		validate: toFormikValidate(studentRegistrationSchema),
+		async onSubmit(values, formikBag) {
+			const result = await createStudent(values);
 			if (result.success) {
-				setErrorMessage(undefined);
-				console.log(result.data);
+				formikBag.setStatus(undefined);
+				router.push(`/students/${result.data}`);
 			} else {
-				setErrorMessage(result.message);
+				formikBag.setStatus(result.message);
 			}
-		} catch (error) {
-			if (error instanceof ZodError) {
-				setIssues(new Map(error.issues.map(issue => [issue.path[0].toString(), issue.message])));
-			}
-		}
-	};
+		},
+	});
 
 	return (
-		<form className={className} action={handleForm}>
-			{errorMessage === undefined ? null : <div className='bg-wRed-200 text-wRed-600 rounded p-2 mb-4'>{errorMessage}</div>}
-			<LabeledInput required name='registration' label='Matrícula' className='mb-4 w-full' issueText={issues.get('registration')}/>
-			<LabeledInput required name='givenName' label='Nombre(s)' className='mb-4' issueText={issues.get('givenName')}/>
-			<LabeledInput required name='familyName' label='Apellido(s)' className='mb-4' issueText={issues.get('familyName')}/>
-			<ButtonModalTrigger label='Registrar datos biometricos'>
+		<form className={className} onSubmit={formik.handleSubmit}>
+			{
+				formik.status && (
+					<div className='bg-red-600 text-stone-400'>
+						{formik.status}
+					</div>
+				)
+			}
+
+			{errorMessage === undefined ? null
+				: <div className='bg-wRed-200 text-wRed-600 rounded p-2 mb-4'>{errorMessage}</div>}
+			<TextField
+				isRequired id='registration'
+				label='Matrícula'
+				className='mb-4 w-full'
+				value={formik.values.registration}
+				errorMessage={formik.errors.registration}
+				onChange={async value => formik.setFieldValue('registration', value, false)}/>
+			<TextField
+				isRequired label='Nombre(s)'
+				className='mb-4'
+				value={formik.values.givenName}
+				errorMessage={formik.errors.givenName}
+				onChange={async value => formik.setFieldValue('givenName', value, false)}
+			/>
+			<TextField
+				isRequired label='Apellido(s)'
+				className='mb-4'
+				value={formik.values.familyName}
+				errorMessage={formik.errors.givenName}
+				onChange={async value => formik.setFieldValue('familyName', value, false)}
+			/>
+			<p className='text-sm mb-2'>
+				{
+					formik.values.biometricData === undefined
+						? 'No se ha agregado información biometrica.'
+						: 'Información biometrica agregada correctamente.'
+				}
+			</p>
+
+			{formik.errors.biometricData && (
+				<div className='text-red-400 text-xs mb-2'>
+					{formik.errors.biometricData}
+				</div>
+			)}
+
+			<ButtonModalTrigger
+				label={
+					<>
+						<Icon name='add' className='me-1' size='sm'/>
+						{
+							formik.values.biometricData === undefined
+								? 'Agregar datos biometricos'
+								: 'Cambiar datos biometricos'
+						}
+					</>
+				} color='secondary' size='sm'
+				variant='text'
+				className='w-full mb-4'
+			>
 				{
 					close => (
 						<BiometricDataDialog
 							close={close}
-							onBiometricDataSubmission={setBiometricData}
-						/>
+							onBiometricDataSubmission={biometricData => {
+								void formik.setFieldValue('biometricData', biometricData);
+							}}/>
 					)
 				}
 			</ButtonModalTrigger>
 
-			<h2>
-				Nuevo Tutor
-			</h2>
-			{/* <input
-				type='text'
-				id='tutor'
-				className='bg-stone-700'
-				onChange={async event => {
-					console.log(await getCertainTutors('Erick'));
-				}}
-			/> */}
-			<LabeledInput required name='tutorGivenName' label='Nombre(s)' className='mb-4 w-full' issueText={issues.get('tutorGivenName')}/>
-			<LabeledInput required name='tutorFamilyName' label='Apellido(s)' className='mb-4' issueText={issues.get('tutorFamilyName')}/>
-			<LabeledInput required name='tutorEmail' label='Correo' className='mb-4' issueText={issues.get('tutorEmail')}/>
-			<LabeledInput required name='tutorPhone' label='Teléfono' className='mb-4' issueText={issues.get('tutorPhone')}/>
 			<div className='flex justify-between'>
+				<Button
+					color='secondary' size='sm'
+					variant='outlined'
+					onPress={() => {
+						router.push('/students');
+					}}
+				>Cancelar</Button>
 				<Button color='secondary' type='submit' size='sm'>Confirmar</Button>
-				<Button color='destructive' type='reset' size='sm'>Cancelar</Button>
 			</div>
 		</form>
 	);
