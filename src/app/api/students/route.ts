c92d58a0dc57af78a7da1e878c70ba07ebf45886
Erick Hernández <erickhdz17@gmail.com>
2023-10-12 07:41:13 -0600

    Student info and table update

---
+++ src/app/api/students/route.ts
@@ -0,0 +1,6 @@
+import {withApiAuthRequired} from '@auth0/nextjs-auth0';
+import {NextResponse} from 'next/server';
+import {getAllStudents} from '@/lib/student.ts';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const GET = withApiAuthRequired(async () => NextResponse.json(await getAllStudents()));
