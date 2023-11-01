dc222998961b864421e0065c0ddee532c35ca63f
Erick Hernández <erickhdz17@gmail.com>
2023-11-01 09:49:06 -0600

    Fixed student and tutor registration

--- src/app/students/create/create-student-action.ts
+++ src/app/students/create/create-student-action.ts
@@ -8,20 +8,22 @@ export default async function createStudent(formData: FormData): Promise<ServerA
 	try {
 		const registrationData = await decodeForm(formData, studentRegistrationSchema);
 
-		const tutor = await prisma.tutor.create({
-			data: {
-				givenName: registrationData.tutorGivenName,
-				familyName: registrationData.tutorFamilyName,
-				phoneNumber: registrationData.tutorPhone,
-				email: registrationData.tutorEmail,
-			},
-		});
 		const student = await prisma.student.create({
 			data: {
 				registration: registrationData.registration,
 				givenName: registrationData.givenName,
 				familyName: registrationData.familyName,
 				biometricData: Buffer(8),
+				tutors: {
+					create: [
+						{
+							givenName: registrationData.tutorGivenName,
+							familyName: registrationData.tutorFamilyName,
+							phoneNumber: registrationData.tutorPhone,
+							email: registrationData.tutorEmail,
+						},
+					],
+				},
 			},
 		});
 		return {
