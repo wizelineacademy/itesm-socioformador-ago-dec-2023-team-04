da07207241171fcf1537958e80122cf0887abbc5
Raúl <a01197655@tec.mx>
2023-11-07 11:56:43 -0600

    Statistics demo view

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,7 +1,29 @@
+'use client';
 import React from 'react';
+import {Input} from '@/components/input.tsx';
+import {RadioGroup, Radio} from '@/components/radio.tsx';
+import {DatePicker} from '@/components/date-picker.tsx';
 
 export default function Home() {
 	return (
-		<main className='w-full'/>
+		<main className='flex flex-col h-full text-stone-400'>
+			<div className='flex items-top mb-4 gap-4'>
+				<h1 className='text-4xl text-stone-50'>
+					Estadísticas
+				</h1>
+			</div>
+			<div className='flex flex-col gap-4 h-full'>
+				<div className='bg-stone-800 grow rounded'>
+					<RadioGroup label='Seleccione uno'>
+						<Radio value='entry'>Registro de entradas</Radio>
+						<Radio value='exit'>Registro de salidas</Radio>
+					</RadioGroup>
+					Tiempo <Input className='w-1/4'/>
+					<DatePicker/>
+					Grupo(s): <Input type='text' className='w-1/2' placeholder='Grupos...'/>
+					<div>Gráfica</div>
+				</div>
+			</div>
+		</main>
 	);
 }
