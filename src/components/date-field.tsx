807eb2e95a87786ed116e2af6f2945a5b37bb587
Raúl <a01197655@tec.mx>
2023-11-10 09:02:59 -0600

    date range picker

--- src/components/date-field.tsx
+++ src/components/date-field.tsx
@@ -1,18 +1,16 @@
-import React, {type ReactNode} from 'react';
-import {type AriaDateFieldProps, useDateField, useDateSegment, useLocale} from 'react-aria';
-import {type DateFieldState, type DateSegment as DateSegmentProp, useDateFieldState} from 'react-stately';
+import React from 'react';
+import {type AriaDateFieldProps, useDateField, useLocale} from 'react-aria';
+import {useDateFieldState} from 'react-stately';
 import {createCalendar, type DateValue} from '@internationalized/date';
+import DateSegment from '@/components/date-segment.tsx';
+import {cx} from '@/lib/cva.ts';
 
 export type DateFieldProps = {
-	readonly label?: ReactNode;
-	readonly field?: ReactNode;
-	readonly input?: ReactNode;
-	readonly description?: ReactNode;
-	readonly errorMessage?: ReactNode;
 	readonly className?: string;
 } & AriaDateFieldProps<DateValue>;
 
 export default function DateField(props: DateFieldProps) {
+	const {className} = props;
 	const {locale} = useLocale();
 	const state = useDateFieldState({
 		...props,
@@ -24,7 +22,7 @@ export default function DateField(props: DateFieldProps) {
 	const {labelProps, fieldProps} = useDateField(props, state, ref);
 
 	return (
-		<div className='w-fit group'>
+		<div className={cx('w-fit group', className)}>
 			<span
 				{...labelProps}
 				className='block group-focus-within:text-stone-50 text-stone-400 text-xs mb-1'
@@ -39,26 +37,6 @@ export default function DateField(props: DateFieldProps) {
 				))}
 				{state.isInvalid && <span aria-hidden='true'>🚫</span>}
 			</div>
-		</div>
-	);
-}
-
-type DateSegmentProps = {
-	readonly segment: DateSegmentProp;
-	readonly state: DateFieldState;
-};
-
-function DateSegment({segment, state}: DateSegmentProps) {
-	const ref = React.useRef(null);
-	const {segmentProps} = useDateSegment(segment, state, ref);
-
-	return (
-		<div
-			{...segmentProps}
-			ref={ref}
-			className={`segment ${segment.isPlaceholder ? 'placeholder' : ''}`}
-		>
-			{segment.text}
 		</div>
 	);
 }
