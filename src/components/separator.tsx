d42a1b762b1af3baadcbdd73cb0745378bc46f26
Raúl <a01197655@tec.mx>
2023-11-13 10:27:24 -0600

    clcx to cx

--- src/components/separator.tsx
+++ src/components/separator.tsx
@@ -2,12 +2,12 @@
 
 import React from 'react';
 import * as BaseSeparator from '@radix-ui/react-separator';
-import clsx from 'clsx';
+import {cx} from '@/lib/cva.ts';
 
 export default function Separator({
 	orientation,
 	className,
 	...props
 }: BaseSeparator.SeparatorProps) {
-	return <BaseSeparator.Root {...props} orientation={orientation} className={clsx(className, orientation === 'horizontal' && 'my-2 w-full', orientation === 'vertical' && 'mx-2 h-full', 'bg-stone-800')}/>;
+	return <BaseSeparator.Root {...props} orientation={orientation} className={cx(className, orientation === 'horizontal' && 'my-2 w-full', orientation === 'vertical' && 'mx-2 h-full', 'bg-stone-800')}/>;
 }
