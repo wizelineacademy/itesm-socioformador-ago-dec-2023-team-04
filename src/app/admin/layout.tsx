7c643432b5db8279c3bd61a8bcc0f3d93af11d2c
Raúl <a01197655@tec.mx>
2023-11-13 14:24:42 -0600

    conflict solved

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {getAllUsers} from '@/lib/user.ts';
+import UserAdminClientLayout from '@/app/admin/users/user-admin-client-layout.tsx';
+
+export default async function UsersLayout({children}: {children: React.ReactNode}) {
+	const users = await getAllUsers();
+	return (
+		<UserAdminClientLayout initialUsers={users}>
+			{children}
+		</UserAdminClientLayout>
+	);
+}
