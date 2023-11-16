14c671c3ba58cadea7b4c4053f9a0e8e480e8751
Raul Isai Murillo Aleman <A01197655@tec.mx>
2023-11-16 08:29:17 -0600

    components

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,7 +1,34 @@
+'use client';
 import React from 'react';
+import {type DateValue} from '@internationalized/date';
+import {RadioGroup, Radio} from '@/components/radio.tsx';
+import DateRangePicker from '@/components/date-range-picker.tsx';
+import {dateSchema} from '@/lib/statistics.ts';
+import Chart from '@/components/chart.tsx';
 
 export default function Home() {
 	return (
-		<main className='w-full'/>
+		<main className='flex flex-col h-full text-stone-400'>
+			<div className='flex items-top mb-4 gap-4'>
+				<h1 className='text-4xl text-stone-50'>
+					Estadísticas
+				</h1>
+			</div>
+			<div className='flex flex-col gap-4 h-full p-4'>
+				<div className='bg-stone-800 grow rounded'>
+					<RadioGroup label='Seleccione uno'>
+						<Radio value='entry'>Registro de entradas</Radio>
+						<Radio value='exit'>Registro de salidas</Radio>
+						<Radio value='attendence'>Registro de asistencias</Radio>
+						<Radio value='entryHours'>Horas de entrada</Radio>
+						<Radio value='exitHours'>Horas de salida</Radio>
+					</RadioGroup>
+					<DateRangePicker
+						label='Seleccione un intérvalo de fechas'/>
+					<h2> Gráfico </h2>
+					<Chart/>
+				</div>
+			</div>
+		</main>
 	);
 }
---
+++ src/components/search-field.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {useSearchFieldState, type SearchFieldProps as SearchFieldStateProps} from 'react-stately';
+import {type AriaSearchFieldProps, type SearchFieldAria, useSearchField} from 'react-aria';
+import Icon from '@/components/icon.tsx';
+import {cx} from '@/lib/cva.ts';
+import {Button} from '@/components/button.tsx';
+
+export type SearchFieldProps = {
+	readonly className?: string;
+} & AriaSearchFieldProps & SearchFieldStateProps;
+
+export default function SearchField(props: SearchFieldProps) {
+	const {className} = props;
+	const state = useSearchFieldState(props);
+	const ref = React.useRef(null);
+	const {inputProps, clearButtonProps} = useSearchField(props, state, ref);
+
+	return (
+		<div className={cx('bg-stone-700 h-10 rounded flex items-center justify-between p-1 gap-1', className)}>
+			<Icon name='search' className='text-stone-400'/>
+			<input
+				{...inputProps} ref={ref} className='bg-transparent  outline-none  h-fit border-none appearance-none'
+				placeholder='Buscar'/>
+			{
+				state.value === ''
+					? <div className='bg-stone-800 text-stone-400 rounded align-middle text-sm py-1 px-2'>ctrl K</div>
+					: <Button {...clearButtonProps} variant='text' color='tertiary' size='xs'><Icon name='close'/></Button>
+			}
+		</div>
+	);
+}
