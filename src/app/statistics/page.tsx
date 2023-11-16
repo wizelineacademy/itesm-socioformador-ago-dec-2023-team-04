64d949253f10e37dbd08fa2a6cf2a73f6373edfa
Raul Isai Murillo Aleman <A01197655@tec.mx>
2023-11-16 08:29:17 -0600

    components

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,33 +1,32 @@
+'use client';
 import React from 'react';
-import StatisticsClient from '@/app/statistics/statistics-client.tsx';
-import {getUserFromSession, getGroupsWithUserId} from '@/lib/user.ts';
-import Chart from "@/app/statistics/chart.tsx";
+import {type DateValue} from '@internationalized/date';
+import {RadioGroup, Radio} from '@/components/radio.tsx';
+import DateRangePicker from '@/components/date-range-picker.tsx';
+import {dateSchema} from '@/lib/statistics.ts';
+import Chart from '@/components/chart.tsx';
 
-export default async function Page() {
-	const userId = await getUserFromSession();
-
-	if (userId === null) {
-		throw new Error('User not found');
-	}
-
-	const groupList = await getGroupsWithUserId(userId.id);
-
-	if (groupList === null) {
-		throw new Error('Groups not found');
-	}
-
-	const time: Date[] = [new Date('2023-11-26'), new Date('2023-11-27')];
-
+export default function Home() {
 	return (
 		<main className='flex flex-col h-full text-stone-400'>
 			<div className='flex items-top mb-4 gap-4'>
 				<h1 className='text-4xl text-stone-50'>
 					Estadísticas
 				</h1>
-				<div>
-					<div>
-						<StatisticsClient groups={groupList.groups}/>
-					</div>
+			</div>
+			<div className='flex flex-col gap-4 h-full p-4'>
+				<div className='bg-stone-800 grow rounded'>
+					<RadioGroup label='Seleccione uno'>
+						<Radio value='entry'>Registro de entradas</Radio>
+						<Radio value='exit'>Registro de salidas</Radio>
+						<Radio value='attendence'>Registro de asistencias</Radio>
+						<Radio value='entryHours'>Horas de entrada</Radio>
+						<Radio value='exitHours'>Horas de salida</Radio>
+					</RadioGroup>
+					<DateRangePicker
+						label='Seleccione un intérvalo de fechas'/>
+					<h2> Gráfico </h2>
+					<Chart/>
 				</div>
 			</div>
 		</main>
