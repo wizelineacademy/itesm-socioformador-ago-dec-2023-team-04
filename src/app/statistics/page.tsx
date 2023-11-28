73beb43730123fd7f17648d383a1de272d4e4102
Raul Isai Murillo Aleman <A01197655@tec.mx>
2023-11-27 20:12:43 -0600

    more graph stuff

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import StatisticsClient from '@/app/statistics/statistics-client.tsx';
 import {getUserFromSession, getGroupsWithUserId} from '@/lib/user.ts';
-import Chart from "@/app/statistics/chart.tsx";
+import StatisticsServer from "@/app/statistics/statistics-server.tsx";
+import {AttendanceType} from "@prisma/client";
+import {attendanceAbsence, attendanceJustificatedAbsence, attendanceLate, attendanceOnTime} from "@/lib/attendance.ts";
+import StatisticsBarChart from "@/app/statistics/statistics-bar-chart.tsx";
+
+function formatDataForChart(data: Array<{ attendanceDate: Date; _count: { type: number } }>) {
+	return data.map(entry => ({
+		date: entry.attendanceDate,
+		count: entry._count.type,
+	}));
+}
 
 export default async function Page() {
 	const userId = await getUserFromSession();
@@ -16,7 +26,36 @@ export default async function Page() {
 		throw new Error('Groups not found');
 	}
 
-	const time: Date[] = [new Date('2023-11-26'), new Date('2023-11-27')];
+	const info: string = 'ON_TIME';
+	const groupId: number = 1;
+	const dateRange: Date[] = [new Date('0000-00-00')];
+
+	let data: any[] = [];
+	switch (info) {
+		case AttendanceType.ON_TIME: {
+			data = formatDataForChart(await attendanceOnTime(groupId, dateRange));
+			break;
+		}
+
+		case AttendanceType.LATE: {
+			data = formatDataForChart(await attendanceLate(groupId, dateRange));
+			break;
+		}
+
+		case 'absence': {
+			data = formatDataForChart(await attendanceAbsence(groupId, dateRange));
+			break;
+		}
+
+		case AttendanceType.JUSTIFICATED_ABSENCE: {
+			data = formatDataForChart(await attendanceJustificatedAbsence(groupId, dateRange));
+			break;
+		}
+
+		default: {
+			break;
+		}
+	}
 
 	return (
 		<main className='flex flex-col h-full text-stone-400'>
@@ -24,11 +63,14 @@ export default async function Page() {
 				<h1 className='text-4xl text-stone-50'>
 					Estadísticas
 				</h1>
+
+			</div>
+			<div>
 				<div>
-					<div>
-						<StatisticsClient groups={groupList.groups}/>
-					</div>
+					<StatisticsClient groups={groupList.groups}/>
 				</div>
+				<h2> Gráfico </h2>
+				<StatisticsBarChart data={data}/>
 			</div>
 		</main>
 	);
