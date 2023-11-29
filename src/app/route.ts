d42c824850302da48df630e81830ea2e2d7a9369
Hiram Muñoz <hirammunozr@outlook.com>
2023-11-29 09:08:35 -0600

    finished

--- src/app/route.ts
+++ src/app/route.ts
@@ -1,10 +1,4 @@
-import {type Metadata} from 'next';
 import {redirect} from 'next/navigation';
-
-export const metadata: Metadata = {
-	title: 'Inicio | SATS',
-	description: 'metaphora. Student Attendance Tracking System',
-};
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export function GET() {
